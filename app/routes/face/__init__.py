8ce39f22f97eb5b39f7d7cd60d220037712be2ec
namTh <tranhuunam23022000@gmail.com>
2024-04-15 23:41:05 +0700

    u

--- app/routes/face/__init__.py
+++ app/routes/face/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint, redirect, url_for, flash, render_template, request
 import logging
 
 from app.extensions import limiter
-from app.service.face import detectFace
+from app.service.face import detectFace, findFace
 
 face_bp = Blueprint("face", __name__, url_prefix="/face")
 
@@ -14,3 +14,8 @@ face_bp = Blueprint("face", __name__, url_prefix="/face")
 def handle_face_request():
     request.timeout = 120
     return detectFace(request)
+
+@face_bp.route("/find", methods=["GET", "POST"])
+@limiter.limit("30/minute")
+def find():
+    return findFace(request)
--- app/service/face.py
+++ app/service/face.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import os
 import cv2
 from threading import Thread
+from deepface import DeepFace
 
 UPLOAD_FOLDER = 'uploads'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -32,22 +33,47 @@ def detectFace(request):
     imRGB = cv2.cvtColor(imBRG, cv2.COLOR_BGR2RGB)
 
     obj = RetinaFace.detect_faces(file_path)
-    
+
     for key in obj.keys():
         identity = obj[key]
         facial_area = identity["facial_area"]
 
-        cropped_face = imRGB[facial_area[1]                             :facial_area[3], facial_area[0]:facial_area[2]]
+        cropped_face = imRGB[facial_area[1]
+            :facial_area[3], facial_area[0]:facial_area[2]]
 
         cropped_face_rgb = cv2.cvtColor(cropped_face, cv2.COLOR_BGR2RGB)
 
         # Lưu khuôn mặt đã cắt
         cv2.imwrite(os.path.join(output_folder, str(key) + "__" +
                                  file.filename), cropped_face_rgb)
-        print("done")
     t = Thread(target=remove_file, args=(file_path,))
     t.start()
     return {
         "code": 200,
         "data": "success"
     }
+
+
+def findFace(request):
+    if 'file' not in request.files:
+        return 'Không tìm thấy file'
+
+    file = request.files['file']
+    if file.filename == '':
+        return 'Không tìm thấy tên file'
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    file.save(file_path)
+
+    images = DeepFace.find(img_path=file_path, db_path=output_folder)
+
+    res = []
+    for image in images:
+
+        if (float(image["distance"]) < 0.15):
+            print("---")
+            res.append(image.values[0][0])
+
+    t = Thread(target=remove_file, args=(file_path,))
+    t.start()
+    return res
